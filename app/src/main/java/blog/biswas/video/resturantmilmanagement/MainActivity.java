6636d2ba522d678a96519d072d72188fb8a3da41
sushen <sushenbiswasaga@gmail.com>
2019-09-13 16:21:47 +0600

    Restaurant Meal building Project

--- app/src/main/java/blog/biswas/video/resturantmilmanagement/MainActivity.java
+++ app/src/main/java/blog/biswas/video/resturantmilmanagement/MainActivity.java
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MealBuilder builder = new MealBuilder();
+
+        Meal vegMeal = builder.makeVegMeal();
+        vegMeal.ShowMeal();
+        System.out.println("Total Cost:" + vegMeal.getCost());
+
+        Meal NonvegMeal = builder.makeNonVegMeal();
+        NonvegMeal.ShowMeal();
+        System.out.println("Total Cost:" + NonvegMeal.getCost());
+
     }
 }
